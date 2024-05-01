e1fa4470cb61146592f69e01c1cd724247842ca0
Natalie Shif <natalia.shif@gmail.com>
2024-05-01 11:52:30 +0300

    added to check-triangle

--- js/check-triangle.js
+++ js/check-triangle.js
@@ -6,14 +6,18 @@ const validationSpan = document.querySelector('#validation');
 const totalValidationSpan = document.querySelector('#total-validation');
 const resultSpan = document.querySelector('#result');
 
-function checkNum() {
-    let num = inpA.value;
+function checkNum(inpNum) {
+    let num = inpNum;
 
     // falsy values: false, undefined, [], null, '', 0
     // isNaN() returns "true" if "num" cannot be converted to a number
     if (num === '' || isNaN(num)) {
         validationSpan.innerText = `Input A and is not a number`
     } else {
+        // num = parseInt(num) converts it to an integer
+        // price = parseFloat('3.99sheq') // price is 3.99 now
+        // num = parseFloat(num) converts it to a number with point
+        // num = Number(num) - like +num
         num = +num;
         
         if (num <= 0) {
