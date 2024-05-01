28140c11698e36c51f3f9c9f1264bc735478f074
Natalie Shif <natalia.shif@gmail.com>
2024-05-01 11:28:36 +0300

    added to check-triangle

--- js/check-triangle.js
+++ js/check-triangle.js
@@ -5,3 +5,29 @@ const inpC = document.querySelector('#c');
 const validationSpan = document.querySelector('#validation');
 const totalValidationSpan = document.querySelector('#total-validation');
 const resultSpan = document.querySelector('#result');
+
+function checkNum() {
+    let num = inpA.value;
+
+    // falsy values: false, undefined, [], null, '', 0
+    // isNaN() returns "true" if "num" cannot be converted to a number
+    if (num === '' || isNaN(num)) {
+        validationSpan.innerText = `Input A and is not a number`
+    } else {
+        num = +num;
+        
+        if (num <= 0) {
+
+            validationSpan.innerText = `Input A and is NOT POSITIVE`
+            
+        } else {
+            
+            validationSpan.innerText = `Input A and is SABABA`
+
+        }
+
+    }
+
+
+
+}
