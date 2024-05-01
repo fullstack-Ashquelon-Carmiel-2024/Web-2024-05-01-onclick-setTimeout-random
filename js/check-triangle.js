9ad1accb29bd4da16783df18d47b56b44be79d91
Natalie Shif <natalia.shif@gmail.com>
2024-05-01 10:58:26 +0300

    added to check-triangle

---
+++ js/check-triangle.js
@@ -0,0 +1,7 @@
+const inpA = document.querySelector('#a');
+const inpB = document.querySelector('#b');
+const inpC = document.querySelector('#c');
+
+const validationSpan = document.querySelector('#validation');
+const totalValidationSpan = document.querySelector('#total-validation');
+const resultSpan = document.querySelector('#result');
